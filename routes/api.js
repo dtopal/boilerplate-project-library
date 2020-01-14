aa1dedd206cfd09e4ee8bc4df3fb35f5d9f3683d
dtopal <11358089+dtopal@users.noreply.github.com>
2020-01-14 16:21:59 +0900

    Add API route logic

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@
 *
 *
 *       Complete the API routing below
-*       
-*       
+*
+*
 */
 
 'use strict';
@@ -16,38 +16,115 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 
 module.exports = function (app) {
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      var title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+  MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
+    console.log('DB connection successful');
+    var collection = db.collection('books');
+
+    app.route('/api/books')
+      .get(function (req, res){
+        //response will be array of book objects
+        //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+        collection.find({}).toArray((err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(data);
+          }
+        });
+
+      })
+
+      .post(function (req, res){
+        var title = req.body.title;
+        //response will contain new book object including atleast _id and title
+        if (title == undefined || title == '') {
+          res.send('Missing title');
+          return;
+        }
+        collection.insertOne({
+          'title': title,
+          commentcount: 0,
+          comments: [],
+        }, (err, data) => {
+          res.json(data.ops[0]);
+        });
+
+      })
+
+      .delete(function(req, res){
+        //if successful response will be 'complete delete successful'
+        collection.deleteMany({}, (err, data) => {
+          if (err) {
+            console.log(err);
+            res.send('complete delete unsuccessful');
+          } else {
+            res.send('complete delete successful');
+          }
+        });
+      });
+
+
+
+    app.route('/api/books/:id')
+      .get(function (req, res){
+        var bookid = req.params.id;
+        //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+        collection.findOne({ _id: ObjectId(bookid) }, (err, doc) => {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            if (doc == null) {
+              res.send('no book exists');
+              return;
+            }
+            var response = { _id: doc._id, title: doc.title, comments: doc.comments };
+            res.json(response);
+          }
+        })
+      })
+
+      .post(function(req, res){
+        var bookid = req.params.id;
+        if (!req.body.comment) {
+          res.send('Missing field');
+          return;
+        }
+        var comment = req.body.comment;
+        //json res format same as .get
+        collection.findOneAndUpdate(
+          { _id: ObjectId(bookid) },
+          { $push: { comments: comment } },
+          { returnOriginal: false },
+          function(err, data){
+            if (err) {
+              console.log(err);
+            } else {
+              res.json(data.value);
+            }
+          }
+        );
+      })
+
+      .delete(function(req, res){
+        var bookid = req.params.id;
+        //if successful response will be 'delete successful'
+        collection.findOneAndDelete({ _id: ObjectId(bookid) }, (err, data) => {
+          if (err) {
+            res.send('could not delete ' + bookid);
+          } else {
+            res.send('delete successful');
+          }
+        });
+      });
+
+
+    //404 Not Found Middleware
+    app.use(function(req, res, next) {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      var bookid = req.params.id;
-      var comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      var bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
-  
+  });
 };
